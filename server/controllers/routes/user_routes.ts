8fa2b11662cc508a4d0263676c5fa5b9d59010d9
oliknight1 <oliknight2014@gmail.com>
2021-10-14 21:29:32 +0100

    Add basic login / register backend functionality

--- server/controllers/routes/user_routes.ts
+++ server/controllers/routes/user_routes.ts
@@ -1,8 +1,32 @@
 import express, { Request, Response } from 'express';
+import { auth } from '../../config/firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 export const user_router = express.Router();
 
-user_router.get( '/', ( _req : Request, res : Response ) => {
-	res.send( 'router works' );
+// TODO: Handle invalid login / register
+
+user_router.post( '/login', ( req : Request, _res : Response ) => {
+	const email : string = req.body.email;
+	const password : string = req.body.password;
+	signInWithEmailAndPassword( auth, email, password )
+		.then( ( user_credential ) => {
+			console.log( user_credential );
+		} )
+	.catch();
+} );
+
+user_router.post( '/register', ( req : Request, _res : Response ) => {
+	const email : string = req.body.email;
+	const password : string = req.body.password;
+	console.log( 'run' );
+	createUserWithEmailAndPassword( auth, email, password )
+		.then( ( user_credential ) => {
+			console.log( user_credential );
+		} )
+	.catch( ( Error ) => {
+		console.log( 'itnhas errored', Error );
+	} );
+
 } );
 
