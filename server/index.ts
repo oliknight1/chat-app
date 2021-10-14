516057dec479dc6491abd4897ad8e3a342d78385
oliknight1 <oliknight2014@gmail.com>
2021-10-14 16:58:24 +0100

    Update server message on startup

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import * as http from 'http';
 import { app } from './app';
 import { PORT } from './utils/config';
 
-const server : http.Server = http.createServer( app )
+const server : http.Server = http.createServer( app );
 server.listen( PORT, () => {
-	 console.log( 'server running on 3003' )
-} )
+	 console.log( `server running on ${ PORT }` );
+} );
