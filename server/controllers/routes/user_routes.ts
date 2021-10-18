5f83d5de731fa3741f3da5287166c8feab4e11b2
oliknight1 <oliknight2014@gmail.com>
2021-10-18 20:56:34 +0100

    Add resonse to user routes for frontend to process

--- server/controllers/routes/user_routes.ts
+++ server/controllers/routes/user_routes.ts
@@ -5,24 +5,27 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } f
 export const user_router = express.Router();
 
 // TODO: Handle invalid login / register
+// TODO: Handle error for user already existing
 
-user_router.post( '/login', ( req : Request, _res : Response ) => {
+user_router.post( '/login', ( req : Request, res : Response ) => {
 	const email : string = req.body.email;
 	const password : string = req.body.password;
 	signInWithEmailAndPassword( auth, email, password )
 		.then( ( user_credential ) => {
-			console.log( user_credential );
+			const uid = user_credential.user.uid;
+			res.status( 200 ).send( uid );
 		} )
 	.catch();
 } );
 
-user_router.post( '/register', ( req : Request, _res : Response ) => {
+user_router.post( '/register', ( req : Request, res : Response ) => {
 	const email : string = req.body.email;
 	const password : string = req.body.password;
 	console.log( 'run' );
 	createUserWithEmailAndPassword( auth, email, password )
 		.then( ( user_credential ) => {
-			console.log( user_credential );
+			const uid = user_credential.user.uid;
+			res.status( 201 ).send( uid );
 		} )
 	.catch( ( Error ) => {
 		console.log( 'itnhas errored', Error );
