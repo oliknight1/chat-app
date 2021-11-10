d5e7d3b1941e351fd4575bcf108598403d3a3401
oliknight1 <oliknight2014@gmail.com>
2021-11-10 18:50:24 +0000

    Temp fix to lightmode problem, remove on new Chakra release

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
+import ForceLightMode from './components/ForceLightMode';
 
 
 ReactDOM.render(
 	<ChakraProvider>
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>
+		<ForceLightMode>
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		</ForceLightMode>
 	</ChakraProvider>,
 	document.getElementById('root')
 );
