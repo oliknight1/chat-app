2dd5a468de634c754b2fef4164ee4e8d38048db0
oliknight1 <oliknight2014@gmail.com>
2021-11-22 15:08:23 +0000

    Update firebase config setup

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,24 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import {
-	REACT_APP_FIREBASE_API_KEY,
-	REACT_APP_FIREBASE_AUTH_DOMAIN,
-	REACT_APP_FIREBASE_PROJECT_ID,
-	REACT_APP_FIREBASE_STORAGE_BUCKET,
-	REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	REACT_APP_FIREBASE_APP_ID
-} from '../utils/config';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-	apiKey: REACT_APP_FIREBASE_API_KEY,
-	authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
-	projectId: REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	appID: REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyDwM42BAWw2orsr58Q4VULe9jdlIR66loI",
+    authDomain: "picchat-6f594.firebaseapp.com",
+    projectId: "picchat-6f594",
+    storageBucket: "picchat-6f594.appspot.com",
+    messagingSenderId: "394003318257",
+    appId: "1:394003318257:web:c121a92e74ad87d12cb050"
 };
 
-
 const app = initializeApp( firebaseConfig );
+export const db = getFirestore();
 export const auth = getAuth( app );
 
