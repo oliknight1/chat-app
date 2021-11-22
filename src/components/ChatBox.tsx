97c74f6246f5aace6e6809b6f601e0b05ff523d1
oliknight1 <oliknight2014@gmail.com>
2021-11-22 15:45:26 +0000

    Post message to database

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,5 +1,7 @@
 import {Box, Input} from "@chakra-ui/react";
+import {addDoc, collection, serverTimestamp} from "firebase/firestore";
 import {ChangeEvent, useState} from "react";
+import {db} from "../config/firebase";
 import {useAuth} from "../contexts/auth_context";
 import {Message} from "../utils/typings";
 import ChatMessage from "./ChatMessage";
@@ -9,21 +11,30 @@ const ChatBox = () => {
 	const [ new_message, set_new_message ] = useState<string>( '' );
 	const { current_user } = useAuth();
 	const { uid } = current_user;
-	
-	
-	const post_message = ( e : ChangeEvent<HTMLFormElement>) => {
+
+	const post_message = async ( data : Message ) => {
+    try {
+		const docRef = await addDoc(collection(db, 'messages'), data );
+        console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
+
+	}
+
+	const message_form_handler = ( e : ChangeEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		if( new_message.length > 0 ) {
 			const message: Message = {
 				text: new_message.trim(),
-				// REPLACE WITH TIMESTAMP FROM FIREBASE
-				timestamp: Date.now() ,
+				timestamp: serverTimestamp() ,
 				uid,
 			}
 			set_messages( messages.concat( [message] ) );
-			set_new_message( '' )
+			set_new_message( '' );
+			post_message( message );
 		}
-		
+
 	}
 	const handle_new_message = ( e : ChangeEvent<HTMLInputElement>) => {
 		e.preventDefault();
@@ -40,7 +51,7 @@ const ChatBox = () => {
 					} )
 				}
 			</Box>
-			<form onSubmit={ post_message }>
+			<form onSubmit={ message_form_handler }>
 				<Input type='text' background='green' value={ new_message } onChange={ handle_new_message } />
 			</form>
 		</Box>
--- src/utils/typings.d.ts
+++ src/utils/typings.d.ts
@@ -6,7 +6,7 @@ export interface AppContext {
 
 export interface Message {
 	text : string,
-	timestamp: number,
+	timestamp: FieldValue,
 	uid : string
 }
 
