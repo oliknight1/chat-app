dd9b69c0060bd8c7917db5313f25dc864a3a9f7f
oliknight1 <oliknight2014@gmail.com>
2021-11-23 21:49:30 +0000

    Adjust SendIcon styling

--- src/components/SendIcon.tsx
+++ src/components/SendIcon.tsx
@@ -6,7 +6,7 @@ interface SendIconProps {
 }
 const SendIcon = ( { width , height } : SendIconProps ) => (
 	<Button background='teal.dark' w='fit-content' p={ 2 } borderRadius='full' _hover={{ background: 'teal.dark' }} type='submit'>
-		<svg style={{ width : width, height: height, color: 'white', margin : '0 auto' }} viewBox="0 0 24 24">
+		<svg style={{ width : width, height: height, color: 'white', marginLeft: '1px' }} viewBox="0 0 24 24">
 			<path fill="currentColor" d="M2,21L23,12L2,3V10L17,12L2,14V21Z" />
 		</svg>
 	</Button>
