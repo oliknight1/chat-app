8aab710a612de6c1011f9d5a2e7622daf8b657b7
oliknight1 <oliknight2014@gmail.com>
2021-11-24 15:52:13 +0000

    Add sidebar with icons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,19 @@
+import { Button, Flex, VStack } from "@chakra-ui/react"
+import { HomeIcon, AddIcon, AccountIcon, LogOutIcon } from "../utils/icons"
 
 const Sidebar = () => {
 	return (
-		<div>sidebar</div>
+		<Flex h='100%' backgroundColor='teal.dark' p={ 8 } flexDir='column' justifyContent='space-between'>
+			<VStack spacing={ 8 }>
+				<HomeIcon boxSize={ 10 } color='white' />	
+				<AddIcon boxSize={ 10 } color='white' />
+
+			</VStack>
+			<VStack spacing={ 8 }>
+				<AccountIcon boxSize={ 10 } color='white' />
+				<Button><LogOutIcon boxSize={ 10 } color='white' /></Button>
+			</VStack>
+		</Flex>
 	)
 }
 export default Sidebar
