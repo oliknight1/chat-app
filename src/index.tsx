189c97cebb9cb7e0ff1c3c20564f61f19e5012f9
oliknight1 <oliknight2014@gmail.com>
2021-12-03 20:39:02 +0000

    Change default Spinner styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme, SpinnerProps } from '@chakra-ui/react';
 
-const theme = extendTheme( {
-	colors: {
-		teal : {
-			dark: '#38B2AC'
+
+const theme = extendTheme(
+	{
+		colors: {
+			teal: {
+				dark: '#38B2AC'
+			}
+		},
+		components:{
+			Spinner: {
+				baseStyle: ( props: SpinnerProps ) => {
+					const { color, thickness, emptyColor } = props
+					return {
+						color,
+						borderWidth: thickness,
+						borderBottomColor: emptyColor,
+						borderLeftColor: emptyColor
+					}
+				},
+				defaultProps: {
+					size: 'xl',
+					thickness: '4px',
+					color: 'teal.dark',
+					emptyColor: 'gray.300'
+				}
+			}
 		}
-	}
-} );
+	},
+);
 
 ReactDOM.render(
 	<ChakraProvider theme={ theme }>
