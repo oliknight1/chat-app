f8a3614d33a99168d1633979918db2af5e8cdc78
oliknight1 <oliknight2014@gmail.com>
2021-12-04 15:33:36 +0000

    Refactor ChatMessage props

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -2,13 +2,14 @@ import { Box, Text } from "@chakra-ui/react";
 
 interface ChatMessageProps {
 	message : string,
-	recieved_message : boolean
+	received : boolean
 }
 
-const ChatMessage = ( { message, recieved_message } : ChatMessageProps ) => {
+const ChatMessage = ( { message, received } : ChatMessageProps ) => {
+	console.log( received )
 	return (
-		<Box alignSelf={ recieved_message ? 'flex-end' : 'flex-start' }>
-			<Box backgroundColor={ recieved_message ? 'teal.dark' : 'gray.400' } my={ 5 } w='fit-content' py={ 3 } px={ 6 } borderRadius='xl' >
+		<Box alignSelf={ received ?  'flex-start' : 'flex-end'}>
+			<Box backgroundColor={ received ? 'gray.400' : 'teal.dark' } my={ 5 } w='fit-content' py={ 3 } px={ 6 } borderRadius='xl' >
 				<Text>{ message }</Text>
 			</Box>
 		</Box>
