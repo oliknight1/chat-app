4d56d69f6ec23a0bcc75bcb1fbc2288e7e13a0a9
oliknight1 <oliknight2014@gmail.com>
2021-12-06 20:07:59 +0000

    Prevent unmounted state update on register

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -45,12 +45,13 @@ const Register = () => {
 					display_name : username.trim(),
 				} );
 			} );
+		set_loading( false );
 			history.push( '/' )
 		} catch( e ) {
 			const code = ( e as firebase.FirebaseError ).code;
 			set_error( handle_auth_code( code ) )
-		}
 		set_loading( false );
+		}
 	}
 	return (
 		<>
